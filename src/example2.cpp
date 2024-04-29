4ce60adb3953347caecefa682d0451aee1cc0475
juelin <juelinliu@umass.edu>
2024-04-29 16:47:29 -0400

    clean up

--- src/example2.cpp
+++ src/example2.cpp
@@ -1,8 +1,9 @@
 #include "cnpy_mmap.h"
-int main() {
-    auto arr = cnpyMmap::npy_load("data.npy");
+int main(int argc, const char * argv[]) {
+    auto input_path = argv[1];
+    auto arr = cnpyMmap::npy_load(input_path);
     auto vec = arr.data<int64_t>();
-    std::cout << "vec: ";
+    std::cout << "input vector: ";
     for (size_t i = 0; i < arr.num_vals; i++) {
         std::cout << vec[i] << " ";
     }
